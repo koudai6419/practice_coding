18ef14dd3620404d5615cb17c92b30a385fbcd55
koudai6419 <38716712+koudai6419@users.noreply.github.com>
2020-07-26 07:13:00 +0900

    hoverによって表示される文章を二段表示
    ul/liタグを使って、hoverによって表示される文章を二段表示にした。
    その都度、レイアウト修正。

--- blog_site_Write/script.js
+++ blog_site_Write/script.js
@@ -1,8 +1,16 @@
 $('.sub-title1').hover(
     function(){
-        $('.sub-title1-1').show();
+        $(this).find('.sub-title1-1').addClass('text-active');
     },
     function(){
-        $('.sub-title1-1').hide();
+        $(this).find('.sub-title1-1').removeClass('text-active');
     }
 );
+// $('.sub-title1').hover(
+//     function(){
+//         $(this).find('.sub-title1-1').show();
+//     },
+//     function(){
+//         $(this).find('.sub-title1-1').hide();
+//     }
+// );
